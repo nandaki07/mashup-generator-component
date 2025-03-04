aa217d4821f50d67b136bcba30a43f5e258208d2
nandaki07 <bhonagiri.4@buckeyemail.osu.edu>
2025-03-03 20:16:13 -0500

    interfaces final

--- doc/03-component-interfaces/03-component-interfaces.md
+++ doc/03-component-interfaces/03-component-interfaces.md
@@ -154,7 +154,6 @@ to see them. If you don't like this workflow, you may try following the
 rebase strategies described [here](https://stackoverflow.com/questions/35790561/working-while-waiting-for-pending-pr)
 and [here](https://stackoverflow.com/questions/18021888/continue-working-on-a-git-branch-after-making-a-pull-request).
 
-<!-- TODO: make a new branch from main then delete this comment -->
 
 ## Assignment Tasks
 
@@ -177,7 +176,6 @@ completed the assignment.
 
 ### Changelog
 
-<!-- TODO: update CHANGELOG then delete this comment -->
 
 At the end of every assignment, you should update the
 [CHANGELOG.md](../../CHANGELOG.md) file found in the root of the project folder.
@@ -212,7 +210,6 @@ request merge (or at least tag your commits). This is not required.
 
 ### Submission
 
-<!-- TODO: read the submission instructions then delete this comment -->
 
 Assuming that your project is in a GitHub repo somewhere and your changes are on
 a proof-of-concept branch, then what we'll want you to do is create a pull
---
+++ src/ProjectTracker.java
@@ -0,0 +1,57 @@
+package components.projecttracker;
+
+/**
+ * {@code ProjectTrackerKernel} enhanced with secondary methods.
+ */
+
+public interface ProjectTracker
+        extends Comparable<ProjectTracker>, ProjectTrackerKernel {
+
+    /**
+     * Calculates the overall progress of a project based on the completed
+     * milestones.
+     *
+     * @updates this
+     * @ensures progress is calculated based on completed milestones
+     */
+    void calculateProgress();
+
+    /**
+     * Sends reminders for approaching milestones.
+     *
+     * @updates this
+     * @ensures reminders are sent to relevant team members
+     */
+    void sendReminders();
+
+    /**
+     * Generates a progress report and exports it as a PDF file.
+     *
+     * @param fileName
+     *            the name of the file to export the progress report to
+     * @requires fileName is a valid string and writable
+     * @ensures a progress report is generated and saved as a PDF file
+     */
+    void generateProgressReport(String fileName);
+
+    /**
+     * Sorts milestones by their approaching deadlines.
+     *
+     * @updates this
+     * @ensures milestones are sorted in order of their deadlines
+     */
+    void sortMilestones();
+
+    /**
+     * Assigns a team member to a specific milestone and makes them the owner.
+     *
+     * @param milestoneName
+     *            the name of the milestone to be assigned
+     * @param owner
+     *            the name of the team member to assign as the owner
+     * @requires milestoneName and owner are valid strings
+     * @ensures the specified milestone is assigned to the given owner
+     */
+    void assignMilestone(String milestoneName, String owner);
+
+}
